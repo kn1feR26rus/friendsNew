55852bef3275add5950d1815e1686115a9608e86
Oleg <kn1feR26rus@yandex.ru>
2022-07-15 14:05:17 +0300

    finish_line

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-
-  let windowWidth = $(window).width()
-
+  let windowWidth = $(window).width();
   let swiper = new Swiper(".swiper", {
     slidesPerView: 1,
 
@@ -11,7 +9,7 @@ $(document).ready(function () {
     },
   });
 
-  const slides = [
+  const data = [
     {
       url: "/assets/images/banner1.png",
       data: 01,
@@ -33,18 +31,17 @@ $(document).ready(function () {
     },
   ];
 
-  const mobileSlides = [
+  const mobileData = [
     {
       url: "/assets/images/banner1.png",
       data: 01,
-      titleText:
-        "Скоро! Новый проект рядом с бульваром Щипакина ",
-      text: "Новое качество жизни в центре событий большого города!",
+      titleText: "Скоро! Новый проект рядом с бульваром Щипакина ",
+      text: "Новое качество жизни<br> в центре событий большого города!",
     },
     {
       url: "/assets/images/banner2.png",
       data: 02,
-      titleText: "Покупать на старте максимально выгодно!",
+      titleText: "Покупать на старте максимально<br> выгодно!",
       text: "Стоимость квадратного метра в наших микрорайонах с момента старта продаж до сдачи дома в среднем вырастает на 20%",
     },
     {
@@ -58,7 +55,6 @@ $(document).ready(function () {
   function displaySlides(data) {
     $(data).each(function (i) {
       let item = data[i];
-
       let slide =
         $(`<div><div class="banner_container-text"><h1 class="banner_title">${item.titleText}</h1>
       <h2 class="banner_subtitle">${item.text}</h2>
@@ -67,43 +63,53 @@ $(document).ready(function () {
           .addClass("swiper-slide banner_background")
           .attr("id", item.data)
           .css("background-image", "url(" + item.url + ")");
-        swiper.appendSlide(slide);
+      swiper.appendSlide(slide);
     });
   }
 
   if (windowWidth > 768) {
-    displaySlides(slides);
+    displaySlides(data);
   } else {
-    displaySlides(mobileSlides);
+    displaySlides(mobileData);
   }
 
-  displaySlides(slides);
-
-  $(".arrow_next").on('click', function () {
-    $("#active_count").text("0" + $(".swiper-slide-active").attr("id"));
-    $(".mobile_active_count").text("0" + $(".swiper-slide-active").attr("id"));
-   
-    $(".banner_navigation-text").each(function(i) {
-        let currentItem = $(".banner_navigation-text")[i]
-        if ($(".swiper-slide-active").attr("id") == $(currentItem).attr('data')) {
-            $(this).addClass('current')
-        } else {
-            $(this).removeClass('current')
-        }
-    })
+  $(".mobile_menu").click(function () {
+    $(this).toggleClass("open");
+    $(".mobile_navigtaion").toggleClass("mobile_menu");
+    $("body").toggleClass("oveflow_hidden");
   });
 
-  $(".arrow_prev").on('click', function () {
+  $(".close_btn").click(function () {
+    $(".mobile_menu").removeClass("open");
+    $(".mobile_navigtaion").removeClass("mobile_menu");
+    $("body").removeClass("oveflow_hidden");
+  });
+
+  $(".arrow_next").on("click", function () {
     $("#active_count").text("0" + $(".swiper-slide-active").attr("id"));
     $(".mobile_active_count").text("0" + $(".swiper-slide-active").attr("id"));
-    
-    $(".banner_navigation-text").each(function(i) {
-        let currentItem = $(".banner_navigation-text")[i]
-        if ($(".swiper-slide-active").attr("id") == $(currentItem).attr('data')) {
-            $(this).addClass('current')
-        } else {
-            $(this).removeClass('current')
-        }
-    })
+
+    $(".banner_navigation-text").each(function (i) {
+      let currentItem = $(".banner_navigation-text")[i];
+      if ($(".swiper-slide-active").attr("id") == $(currentItem).attr("data")) {
+        $(this).addClass("current");
+      } else {
+        $(this).removeClass("current");
+      }
+    });
+  });
+
+  $(".arrow_prev").on("click", function () {
+    $("#active_count").text("0" + $(".swiper-slide-active").attr("id"));
+    $(".mobile_active_count").text("0" + $(".swiper-slide-active").attr("id"));
+
+    $(".banner_navigation-text").each(function (i) {
+      let currentItem = $(".banner_navigation-text")[i];
+      if ($(".swiper-slide-active").attr("id") == $(currentItem).attr("data")) {
+        $(this).addClass("current");
+      } else {
+        $(this).removeClass("current");
+      }
+    });
   });
 });
